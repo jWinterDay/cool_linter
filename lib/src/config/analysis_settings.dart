de9a526f21b1ee492a0942b47cdd46bdbeed5917
jwd25 <jwinterday@mail.ru>
2021-07-17 20:36:06 +0300

    yaml config draft

---
+++ lib/src/config/analysis_settings.dart
@@ -0,0 +1,102 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'analysis_settings.freezed.dart';
+part 'analysis_settings.g.dart';
+
+/// ``` yaml
+/// cool_linter:
+///   always_specify_types:
+///     - typed_literal
+///     - declared_identifier
+///     - set_or_map_literal
+///     - simple_formal_parameter
+///     - type_name
+///     - variable_declaration_list
+///   regexp_exclude:
+///     -
+///       pattern: Colors
+///       hint: Use colors from design system instead!
+///       severity: WARNING
+///   exclude_folders:
+///    - test/**
+/// ```
+///
+/// and map associated with this yaml:
+/// ```
+/// {
+///   cool_linter: {
+///     always_specify_types: [typed_literal, declared_identifier, set_or_map_literal, simple_formal_parameter, type_name, variable_declaration_list],
+///     regexp_exclude: [{pattern: Colors, hint: Use colors from design system instead!, severity: WARNING}],
+///     exclude_folders: [test/**]
+///   }
+/// }
+/// ```
+@freezed
+class AnalysisSettings with _$AnalysisSettings {
+  const AnalysisSettings._();
+
+  const factory AnalysisSettings({
+    @Default(CoolLinter()) @JsonKey(name: 'cool_linter') CoolLinter? coolLinter,
+  }) = _AnalysisSettings;
+
+  factory AnalysisSettings.fromJson(Map<String, dynamic> json) => _$AnalysisSettingsFromJson(json);
+}
+
+@freezed
+class CoolLinter with _$CoolLinter {
+  const CoolLinter._();
+
+  const factory CoolLinter({
+    @Default(<String>[]) @JsonKey(name: 'always_specify_types', defaultValue: <String>[]) List<String> types,
+    @Default(<ExcludeWords>[]) @JsonKey(name: 'regexp_exclude') List<ExcludeWords> regexpExclude,
+    @Default(<String>[]) @JsonKey(name: 'exclude_folders', defaultValue: <String>[]) List<String> excludeFolders,
+  }) = _CoolLinter;
+
+  factory CoolLinter.fromJson(Map<String, dynamic> json) => _$CoolLinterFromJson(json);
+}
+
+// @freezed
+// class AlwaysSpecifyTypes with _$AlwaysSpecifyTypes {
+//   const AlwaysSpecifyTypes._();
+
+//   const factory AlwaysSpecifyTypes({
+//     @Default(<String>[]) List<String> types,
+//   }) = _AlwaysSpecifyTypes;
+
+//   factory AlwaysSpecifyTypes.fromJson(Map<String, dynamic> json) => _$AlwaysSpecifyTypesFromJson(json);
+// }
+
+// @freezed
+// class RegexpExclude with _$RegexpExclude {
+//   const RegexpExclude._();
+
+//   const factory RegexpExclude({
+//     @JsonKey(name: 'exclude_words') List<ExcludeWords>? excludeWords,
+//   }) = _RegexpExclude;
+
+//   factory RegexpExclude.fromJson(Map<String, dynamic> json) => _$RegexpExcludeFromJson(json);
+// }
+
+@freezed
+class ExcludeWords with _$ExcludeWords {
+  const ExcludeWords._();
+
+  const factory ExcludeWords(
+    String pattern, {
+    @Default('') String hint,
+    @Default('WARNING') String severity,
+  }) = _ExcludeWords;
+
+  factory ExcludeWords.fromJson(Map<String, dynamic> json) => _$ExcludeWordsFromJson(json);
+}
+
+// @freezed
+// class ExcludeFolders with _$ExcludeFolders {
+//   const ExcludeFolders._();
+
+//   const factory ExcludeFolders({
+//     @Default(<String>[]) List<String> folders,
+//   }) = _ExcludeFolders;
+
+//   factory ExcludeFolders.fromJson(Map<String, dynamic> json) => _$ExcludeFoldersFromJson(json);
+// }
---
+++ lib/src/config/analysis_settings.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'analysis_settings.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AnalysisSettings _$_$_AnalysisSettingsFromJson(Map<String, dynamic> json) {
+  return _$_AnalysisSettings(
+    coolLinter: json['cool_linter'] == null ? null : CoolLinter.fromJson(json['cool_linter'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$_$_AnalysisSettingsToJson(_$_AnalysisSettings instance) => <String, dynamic>{
+      'cool_linter': instance.coolLinter,
+    };
+
+_$_CoolLinter _$_$_CoolLinterFromJson(Map<String, dynamic> json) {
+  return _$_CoolLinter(
+    types: (json['always_specify_types'] as List<dynamic>?)?.map((e) => e as String).toList() ?? [],
+    regexpExclude:
+        (json['regexp_exclude'] as List<dynamic>).map((e) => ExcludeWords.fromJson(e as Map<String, dynamic>)).toList(),
+    excludeFolders: (json['exclude_folders'] as List<dynamic>?)?.map((e) => e as String).toList() ?? [],
+  );
+}
+
+Map<String, dynamic> _$_$_CoolLinterToJson(_$_CoolLinter instance) => <String, dynamic>{
+      'always_specify_types': instance.types,
+      'regexp_exclude': instance.regexpExclude,
+      'exclude_folders': instance.excludeFolders,
+    };
+
+_$_ExcludeWords _$_$_ExcludeWordsFromJson(Map<String, dynamic> json) {
+  return _$_ExcludeWords(
+    json['pattern'] as String,
+    hint: json['hint'] as String? ?? '',
+    severity: json['severity'] as String? ?? 'WARNING',
+  );
+}
+
+Map<String, dynamic> _$_$_ExcludeWordsToJson(_$_ExcludeWords instance) => <String, dynamic>{
+      'pattern': instance.pattern,
+      'hint': instance.hint,
+      'severity': instance.severity,
+    };

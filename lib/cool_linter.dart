4305c03e37f906609323f18a467d2f0ee85eee42
Rexios <rexios@rexios.dev>
2021-12-18 20:08:56 -0500

    Added a doc comment

--- lib/cool_linter.dart
+++ lib/cool_linter.dart
@@ -3,6 +3,7 @@ import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer_plugin/starter.dart';
 import 'package:cool_linter/src/plugin.dart';
 
+/// Start the analysis plugin
 void start(List<String> args, SendPort sendPort) {
   ServerPluginStarter(
     CoolLinterPlugin(PhysicalResourceProvider.INSTANCE),
